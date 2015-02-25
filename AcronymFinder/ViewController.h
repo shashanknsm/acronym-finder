2ef86196ccc7460e2b5a8c2afd731bfcc0f6cf80
shashanknsm <shashanknsm@gmail.com>
2015-02-25 17:39:34 -0600

    First commit.

---
+++ AcronymFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcronymFinder
+//
+//  Created by SHASHANKT on 25/02/15.
+//  Copyright (c) 2015 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AcronymFinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AcronymFinder
+//
+//  Created by SHASHANKT on 25/02/15.
+//  Copyright (c) 2015 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
