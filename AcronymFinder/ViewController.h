08363925cb1f8a58e0f4dcdaf691719885b46c32
shashanknsm <shashanknsm@gmail.com>
2015-02-26 12:06:46 -0600

    Implemented search

--- AcronymFinder/ViewController.h
+++ AcronymFinder/ViewController.h
@@ -7,8 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AFNetworking.h>
+#import <MBProgressHUD.h>
 
-@interface ViewController : UIViewController
+
+@interface ViewController : UIViewController <UISearchBarDelegate,UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong,nonatomic) AFHTTPRequestOperationManager *manager;
+@property (weak, nonatomic) IBOutlet UITableView *resultsTableView;
+@property (strong,nonatomic) NSMutableArray *resultsArray;
 
 
 @end
--- AcronymFinder/ViewController.m
+++ AcronymFinder/ViewController.m
@@ -8,20 +8,68 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define BASE_URL @"http://www.nactem.ac.uk/software/acromine/dictionary.py/?sf=%@"
+static NSString* cellIdentifier = @"cell";
 
-@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+	
+	self.manager = [AFHTTPRequestOperationManager manager];
+	self.manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/plain"];
+  [self.resultsTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
+	self.resultsArray = [NSMutableArray array];
+ }
+
+# pragma mark - UISearchBar Methods
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+	[self.view endEditing:YES];
+	NSString *searchURL = [NSString stringWithFormat:BASE_URL,searchBar.text];
+	[self shouldDisplayIndicator:YES];
+	
+	__weak typeof(self) weakSelf = self;
+	[self.manager GET:searchURL parameters:nil success:^(AFHTTPRequestOperation *operation, NSDictionary*responseObject) {
+		[weakSelf shouldDisplayIndicator:NO];
+		if ([responseObject count]) {
+			NSArray *responseArray = [[NSArray arrayWithArray:[[responseObject valueForKey:@"lfs"]valueForKey:@"lf"]]objectAtIndex:0];
+			self.resultsArray = [NSMutableArray arrayWithArray:responseArray];
+			[self.resultsTableView reloadData];
+ 		}
+	} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		[weakSelf shouldDisplayIndicator:NO];
+	}];
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+	if (searchText.length == 0) {
+		[self.resultsArray removeAllObjects];
+		[self.resultsTableView reloadData];
+	}
+}
+
+# pragma mark - UITableView Methods
+- (NSInteger)tableView:(UITableView *)tableview numberOfRowsInSection:(NSInteger)section {
+	return [self.resultsArray count];
+}
+
+-(UITableViewCell *)tableView: (UITableView *)tableview cellForRowAtIndexPath:(NSIndexPath *)indexpath {
+	
+	UITableViewCell *cell =[tableview dequeueReusableCellWithIdentifier:cellIdentifier];
+	if (self.resultsArray) {
+	 cell.textLabel.text = [self.resultsArray objectAtIndex:indexpath.row];
+	}
+  return cell;
+}
+
+- (void)shouldDisplayIndicator: (BOOL)display {
+	if (display) {
+		[MBProgressHUD showHUDAddedTo:self.view animated:NO];
+	} else {
+		[MBProgressHUD hideHUDForView:self.view animated:NO];
+	}
 }
 
 @end
